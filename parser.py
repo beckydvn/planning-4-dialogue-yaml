4b1d1271dad92ae87320fdb5b86ee101cd990866
beckydvn <18rldv@queensu.ca>
2022-05-16 21:41:13 -0400

    add second parser

--- parser.py
+++ parser.py
@@ -1,14 +1,65 @@
 from enum import Enum, auto
+from opcode import HAVE_ARGUMENT
 import yaml
 
-class Nest(Enum):
+class PDDLKeys(Enum):
     AND = auto()
     OR = auto()
+    NOT = auto()
+    ONEOF = auto()
+    LABELED_ONEOF = auto()
+
+# have a function that creates PDDL representation within each class
+
+def have(entity):
+    return 
+
+def maybe_have(entity):
+    return f"(maybe_have_{entity.lower()} )"
+
+def not_key(entity):
+    return f"(not({entity.lower()} ))"
+
+# class Have:
+#     def __init__(self, entity) -> None:
+#         self.entity = entity.lower()
+#     def __repr__(self) -> str:
+#         return f"(have_{self.entity} )"
+
+# class MaybeHave():
+#     def __init__(self, entity) -> None:
+#         self.entity = entity.lower()
+#     def __repr__(self) -> str:
+#         return f"(maybe_have_{self.entity} )"
+
+def create_tree(nesting):
+    for key in nesting:
+        if key == "havev ":
+            pass
+
+class And:
+    def __init__(self, out) -> None:
+        self.out = []
+        for o in out:
+            # convert to nests of have, not, maybe objects
+            self.out.append(link(o)(out[o]))
+
+class OneOf:
+    def __init__(self, out) -> None:
+        self.out = []
+        for o in out:
+            # convert to nests of have, not, maybe objects
+            self.out.append(link(o)(out[o]))
 
 # class Outcome:
-#     def __init__(self, nest: Nest) -> None:
-#         self.name = name
-#         self.category = category
+#     def __init__(self, name, node_type, out) -> None:
+#         self.name = out["name"]
+#         self.node_type = 
+#         self.out = []
+#         del out["name"]
+#         for o in out:
+#             # convert to nests of have, not, maybe objects
+#             self.out.append(link(o)(out[o]))
 
 class Action:
     def __init__(self, name, category, entities=None, dialogue=None, precond=None, effects=None) -> None:
@@ -17,11 +68,14 @@ class Action:
         self.entities = entities
         self.dialogue = dialogue
         self.precond = precond
-        self.effects = effects
+        self.effects = []
+        for e in effects:
+            for key, val in e.items():
+                self.effects.append(link(key)(val))
+        # self.effects = [OneOf(e) for e in effects]
 
     @classmethod
     def deserialize(cls, data):
-        """Converts a json object to an Action."""
         return cls(**data)
 
 class AskAction(Action):
@@ -29,20 +83,22 @@ class AskAction(Action):
         super().__init__(name, category, entities=entities, dialogue=dialogue, precond=precond, effects=effects)
         # self.precond = {'not': ''}
 
-def link_constructor(key: str):
+def link(key: str):
     return {
-        "ask-for": AskAction
+        "ask-for": AskAction,
+        "one-of": OneOf,
+        "and": And
     }[key]
 
 def grab_yaml_actions():
-   return yaml.load(open("rasa_gist.yaml", "r"), Loader=yaml.FullLoader).get("actions")
+    return yaml.load(open("rasa_gist.yaml", "r"), Loader=yaml.FullLoader).get("actions")
 
 def create_actions():
     actions = grab_yaml_actions()
     ls = []
     for act_type in actions:
         for action in actions[act_type]:
-            ls.append(link_constructor(act_type).deserialize(action))
+            ls.append(link(act_type).deserialize(action))
     return ls
 
 if __name__ == "__main__":
---
+++ parser_2.py
@@ -0,0 +1,36 @@
+from tkinter import E
+import yaml
+
+def grab_yaml_actions():
+    return yaml.load(open("rasa_gist.yaml", "r"), Loader=yaml.FullLoader).get("actions")
+
+def parse_outcome(outcome):
+    if isinstance(outcome, dict):
+        for key, val in outcome.items():
+            if key == "have":
+                return f"(have_{val})"
+            elif key == "maybe_have":
+                return f"(maybe_have_{val})"
+            elif key == "one-of":
+                return "\n(one-of(\n\t{0}\n)".format('\n\t'.join([parse_outcome(outcome) for outcome in val]))
+            elif key == "forced-follow-up":
+                return '\n\t'.join([f"not(can-do_({act}))" for act in action_set - {val}])
+            else:
+                return f"\n(and({parse_outcome(val)})"
+    else:
+        return f"\nand({outcome})"
+
+action_yaml = grab_yaml_actions()
+action_set = {a["name"] for act_type in action_yaml for a in action_yaml[act_type]}
+pddl = ""
+for act_type in action_yaml:
+    for action in action_yaml[act_type]:
+        pddl += "(:action {0}\n\t:effect ".format(action["name"])
+        if "need" in action.keys():
+            for outcome in action["need"]:
+                pddl += parse_outcome(outcome)
+        if "effects" in action.keys():
+            for outcome in action["effects"]:
+                pddl += parse_outcome(outcome)
+        pddl += "\n)"
+print(pddl)
